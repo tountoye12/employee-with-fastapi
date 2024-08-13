063059aebef4e50682ba18221a2206ea067a9a43
Diallo <mamadouadama609@gmail.com>
2024-08-13 17:26:20 -0500

    Nested Body

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
+
+from typing import Annotated
 
 from models import Item
 
@@ -16,9 +18,9 @@ fake_items_db = [
 async def read_item(skip:int=0, limit:int = 10):
     return fake_items_db[skip: skip + limit]
 
-@app.get("/")
-async def home():
-    return {"mgs": "Test"}
+@app.get("/items")
+async def get_all_items():
+    return items_list
 
 
 items_list = [] 
@@ -28,3 +30,13 @@ items_list = []
 async def add_item(item: Item):
     items_list.append(item)
     return item
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: Annotated[int, Path(title="Id of item to get")],
+                   q: Annotated[str | None, Query(alias="item-query")]= None
+                   ):
+    results = {"item_id": item_id}
+    if q:
+        return  results.update("q", q)
+    return results
--- models.py
+++ models.py
@@ -1,12 +1,20 @@
 from pydantic import BaseModel
 
 
+class Image(BaseModel):
+   url: str
+   name: str
+
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
     tax : float | None = None
 
+    image: Image | None = None
+
 
     def __str__(self) -> str:
-        return self.name
+      return self.name
+
+
